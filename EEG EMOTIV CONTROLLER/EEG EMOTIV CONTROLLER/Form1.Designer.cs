588d634751ac2163beae947e61c5d206f4a5d9d2
Sandy Akbar Dewangga <sssandy.ad@gmail.com>
2016-03-22 05:22:54 +0700

    add chart eeg

--- EEG EMOTIV CONTROLLER/EEG EMOTIV CONTROLLER/Form1.Designer.cs
+++ EEG EMOTIV CONTROLLER/EEG EMOTIV CONTROLLER/Form1.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.chartEeg = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chartEeg)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -37,31 +45,66 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Crimson;
-            this.label1.Location = new System.Drawing.Point(78, 43);
+            this.label1.Location = new System.Drawing.Point(768, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(122, 42);
             this.label1.TabIndex = 0;
             this.label1.Text = "Lucky";
             // 
-            // button1
+            // buttonOpen
             // 
-            this.button1.Location = new System.Drawing.Point(99, 109);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "MAU?";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonOpen.Location = new System.Drawing.Point(12, 17);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 1;
+            this.buttonOpen.Text = "open file";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // chartEeg
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartEeg.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartEeg.Legends.Add(legend1);
+            this.chartEeg.Location = new System.Drawing.Point(12, 85);
+            this.chartEeg.Name = "chartEeg";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartEeg.Series.Add(series1);
+            this.chartEeg.Size = new System.Drawing.Size(878, 332);
+            this.chartEeg.TabIndex = 2;
+            this.chartEeg.Text = "Gelombang EEG";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 46);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Yellow;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(902, 429);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.chartEeg);
+            this.Controls.Add(this.buttonOpen);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartEeg)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,7 +113,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartEeg;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- EEG EMOTIV CONTROLLER/EEG EMOTIV CONTROLLER/Form1.cs
+++ EEG EMOTIV CONTROLLER/EEG EMOTIV CONTROLLER/Form1.cs
@@ -1,20 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace EEG_EMOTIV_CONTROLLER
 {
     public partial class Form1 : Form
     {
+        string eegDataFileName;
+        string[] channelsEeg = {"AF3","F7","F3","FC5","T7","P7","O1","O2","P8","T8","FC6","F4","F8","AF4"};
+        int totalChannels;
+        Dictionary<string, double[]> eegData;
+
+        int counter;
+
         public Form1()
         {
             InitializeComponent();
+
+            timer.Enabled = false;
+            counter = 0;
+
+            chartEeg.Series.Clear();
+            totalChannels = channelsEeg.Count();
+            for(int i=0;i<totalChannels;i++)
+            {
+                chartEeg.Series.Add(channelsEeg[i]);
+                chartEeg.Series[channelsEeg[i]].ChartType = SeriesChartType.Line;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            string[] eegDataFromFile;
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "EEG File (*.csv)|*.csv";
+            DialogResult result= ofd.ShowDialog();
+
+            if(result == DialogResult.OK)
+            {
+                try
+                {
+                    eegDataFileName = ofd.FileName;
+                    eegDataFromFile = File.ReadAllLines(eegDataFileName);
+
+                    int totalRecordsData = eegDataFileName.Count() - 1;
+
+                    eegData = new Dictionary<string, double[]>();
+
+                    for (int i = 0; i < totalChannels; i++)
+                    {
+                        eegData.Add(channelsEeg[i], new double[totalRecordsData]);
+                    }
+
+                    for (int i = 0; i < totalRecordsData; i++)
+                    {
+                        string[] record = eegDataFromFile[i+1].Split(',');
+                        for (int j = 0; j < totalChannels; j++)
+                        {
+                            eegData[channelsEeg[j]][i] = double.Parse(record[j + 2]);
+                        }
+                    }
+                }
+                catch(IOException)
+                {
+                    MessageBox.Show("Error! File is already open by another program.");
+                }
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = true;
+            timer.Interval = 8;
+
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            for(int i=0;i<totalChannels;i++)
+            {
+                DataPoint dp = new DataPoint();
+                dp.SetValueY(eegData[channelsEeg[i]][counter]);
+                chartEeg.Series[i].Points.Add(dp);
+            }
         }
     }
 }
