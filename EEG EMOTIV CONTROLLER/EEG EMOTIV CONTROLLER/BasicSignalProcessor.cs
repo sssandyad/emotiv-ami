b538786daae8962797ee233622c71cff300e68f7
Rahmatullah Hutami Hidayat <rahmatullah.hutami@gmail.com>
2016-05-16 04:05:18 +0700

    test

---
+++ EEG EMOTIV CONTROLLER/EEG EMOTIV CONTROLLER/BasicSignalProcessor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BasicDSP;
+
+namespace EEG_EMOTIV_CONTROLLER
+{
+    class BasicSignalProcessor
+    {
+        public List<double[]> Decomposes(double[] data)
+        {
+            int length = data.Length;
+            double[] dataCombinedDeltaTheta = new double[length]; //delta = 1-3 Hz, theta = 4-7 Hz
+            double[] dataAlpha = new double[length]; //alpha = 8-13 Hz
+            double[] dataBeta = new double[length]; //beta = 14-29 Hz
+            double[] dataGamma = new double[length]; //gamma = 30-50 Hz
+
+            LTISystemChain chainCombinedDeltaTheta = Filter.ButterworthBandPass(0.01, 0.07, 4);
+            LTISystemChain chainAlpha = Filter.ButterworthBandPass(0.08, 0.13, 4);
+            LTISystemChain chainBeta = Filter.ButterworthBandPass(0.14, 0.29, 4);
+            LTISystemChain chainGamma = Filter.ButterworthBandPass(0.30, 0.50, 4);
+
+            for (int i = 0; i < length; i++)
+            {
+                //#region 1. ButterworthBandPass
+                dataCombinedDeltaTheta[i] = chainCombinedDeltaTheta[data[i]];
+                dataAlpha[i] = chainAlpha[data[i]];
+                dataBeta[i] = chainBeta[data[i]];
+                dataGamma[i] = chainGamma[data[i]];
+                //#endregion
+            }
+            List<double[]> result = new List<double[]>();
+            result.Add(dataCombinedDeltaTheta);
+            result.Add(dataAlpha);
+            result.Add(dataBeta);
+            result.Add(dataGamma);
+
+            return result;
+        }
+    }
+}
---
+++ EEG EMOTIV CONTROLLER/EEG EMOTIV CONTROLLER/FFTSignalTransform.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MathNet.Numerics.Transformations;
+
+namespace EEG_EMOTIV_CONTROLLER
+{
+    class FFTSignalTransform
+    {
+        const int power = 5;
+        static int windowsize = (int)Math.Pow(2, power);
+        double[] window = new double[windowsize];
+        int windowLength = 0;
+
+        //window for each channel
+
+        public void Init(int channelCount)
+        {
+            //channelCount RealFourierTransformation objects to be reused each time
+            //channelCount window count 
+        }
+
+        /// <summary>
+        /// Currently only first channel
+        /// </summary>
+        /// <param name="data"></param>
+        public double[] DoWork(double[] data)
+        {
+            //1. wait until window buffer is full
+            if (windowLength < windowsize)
+            {
+                window[windowLength] = data[0];
+                windowLength++;
+
+                return data;
+            }
+            else //window buffer full 
+            {
+                //2. perform fft
+                double[] freqReal, freqImag;
+                RealFourierTransformation rft = new RealFourierTransformation(); // default convention
+                rft.TransformForward(window, out freqReal, out freqImag);
+
+                //3. move left 
+                for (int i = window.Length - 2; i >= 0; i--)
+                    window[i + 1] = window[i];
+
+                //4. set new value to be used next time
+                window[0] = data[0];
+
+                //5. Fill first value for return
+                return freqReal;
+
+
+            }
+        }
+    }
+}
